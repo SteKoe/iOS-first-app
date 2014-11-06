19c9ab956cdb4e44e9b20e2a36e580eca219ea23
Stephan Köninger <mail@stekoe.de>
2014-11-06 16:06:25 +0100

    chore(content): Adds new functionality

--- Our second CCCC App/ViewController.swift
+++ Our second CCCC App/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var lbHello: UILabel!
+    @IBOutlet var tfName: UITextField!
+    @IBOutlet var btnSayHello: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnSayHelloPressed(sender: UIButton) {
+        var name : String = tfName.text
+        lbHello.text = "Hello " + name + "!"
+    }
 }
 
